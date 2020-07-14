e9faa5a94c3879817998b488a6e906e5d3db50c1
sen.wan <sen.wan@neoderm.com.cn>
2020-07-14 10:43:06 +0800

    1.draw quadrangle with GLES

--- app/src/main/java/com/github/zedplayer/gles/ZedRender.kt
+++ app/src/main/java/com/github/zedplayer/gles/ZedRender.kt
@@ -13,9 +13,25 @@ import javax.microedition.khronos.opengles.GL10
 class ZedRender constructor(private val context: Context) : GLSurfaceView.Renderer {
     //1.绘制三角形顶点坐标的数组
     private val vertexData = floatArrayOf(
-        -1.0f, 0.0f,
-        0.0f, 1.0f,
-        1.0f, 0.0f
+//        -1.0f, 0.0f,
+//        0.0f, 1.0f,
+//        1.0f, 0.0f,
+        //四边形第二种方法,共用两个点
+        //奇数:n-2,n-1,n(V3:3-2=V1,3-1=V2,V3)
+        //偶数:n-1,n-2,n(V4:4-1=V3,4-2=V2,V4)
+        //V1->V2->V3和V3->V2->V4图形环绕方向一致
+//        -1.0f, 0.0f,
+//        0.0f, -1.0f,
+//        0.0f, 1.0f,
+//        1.0f, 0.0f
+        0.0f,1.0f,
+        1.0f,0.0f,
+        -1.0f,0.0f,
+        0.0f,-1.0f
+        //四边形第一种方法,按照同时针方向增加至6个点的坐标,绘制两个三角形
+//        -1.0f, 0.0f,
+//        1.0f, 0.0f,
+//        0.0f, -1.0f
     )
     private lateinit var vertexBuffer: FloatBuffer
     //3.三角形shader脚本的编写(res/raw)
@@ -43,7 +59,8 @@ class ZedRender constructor(private val context: Context) : GLSurfaceView.Render
         GLES20.glUniform4f(afColor, 1.0f, 1.0f, 0.0f, 1.0f)
         GLES20.glEnableVertexAttribArray(avPosition)
         GLES20.glVertexAttribPointer(avPosition, 2, GLES20.GL_FLOAT, false, 8, vertexBuffer)
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3)
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4)
+//        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 6)
     }
 
     override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
